174e551d2252f82232492e272acfeb3b9e2b18b3
trongducKT <buitrongducktmr@gmail.com>
2024-01-01 20:30:35 +0700

    Finished

--- BuiTrongDuc/Topic5_GA.py
+++ BuiTrongDuc/Topic5_GA.py
@@ -156,7 +156,7 @@
 #     for pos in se:
 #         print(pos, end = ' ')
 #     print(0)
-
+import numpy as np
 def read_data():
     N, K = list(map(int, input().split()))
     d = [0]
@@ -166,8 +166,14 @@ def read_data():
         t.append(list(map(int, input().split())))
     return N, K, d, t
 
+def creat_cost(N, d, t):
+    cost_matrix = np.zeros((N + 1, N + 1))
+    for i in range(N + 1):
+        for j in range(N + 1):
+            if i != j:
+                cost_matrix[i][j] = d[j] + t[i][j]
 
-import numpy as np
+    return cost_matrix
 
 def cal_cost(router, t, d):
     cost = [0 for i in range(len(router))]
@@ -176,42 +182,40 @@ def cal_cost(router, t, d):
             cost[i] += t[router[i][j]][router[i][j + 1]] + d[router[i][j + 1]]
     return cost
 
-def UCV(visited):
-        unvisited = []
-        for pos in range(len(visited)):
-            if visited[pos] == 0:
-                unvisited.append(pos)
-        return unvisited
-
-def local_search_greedy1():
+def local_search_greedy2():
     N, K, d, t = read_data()
-    router = []
-    for i in range(K):
-        router.append([0])
+    cost_matrix = creat_cost(N, d, t)
 
-    visited = np.zeros(N + 1, dtype = int)
-    visited[0] = 1      
+    router = [[0] for i in range(K)]
+    cost = [0 for i in range(K)]
+    tuple_dis_depot = []
+    for i in range(1, N + 1):
+        tuple_dis_depot.append((i, cost_matrix[0][i]))
+    tuple_dis_depot.sort(key = lambda x: x[1])
+
+    while len(tuple_dis_depot) > 0:
+        # lay phan tu dau tien trong tuple_dis_depot
+        first = tuple_dis_depot[0][0]
+        # xoa phan tu dau tien trong tuple_dis_depot
+        tuple_dis_depot.pop(0)
+        # Them vao router lam tang it chi phi nhat
+
+        cost_value = [cost[i] + cost_matrix[router[i][-1]][first] for i in range(K)]
+        min_cost = min(cost_value)
+
+        index_list = [index for index, value in enumerate(cost_value) if value == min_cost]
+
+        router_selected = index_list[0]
+        for index in index_list[1:]:
+            if cost[index] < cost[router_selected]:
+                router_selected = index
         
-    k = 0
-    while sum(visited) != N + 1:
-        start = router[k][-1]
-        next_pos = 0
-        next_time = 1000000
-        unvisited = UCV(visited)
-        for pos in unvisited:
-            if t[start][pos] + d[pos] < next_time:
-                next_time = t[start][pos] + d[pos]
-                next_pos = pos
-        router[k].append(next_pos)
-        visited[next_pos] = 1
-        k = (k + 1) % K
-    
+        router[router_selected].append(first)
+        cost[router_selected] += cost_matrix[router[router_selected][-2]][first]
+
     for i in range(K):
         router[i].append(0)
-    cost = [0 for i in range(K)]
-    for i in range(K):
-        for j in range(len(router[i]) - 1):
-            cost[i] += t[router[i][j]][router[i][j + 1]] + d[router[i][j + 1]]
+        cost[i] += cost_matrix[router[i][-2]][0]
     history_count = 0
     while True:
         max_cost = max(cost)
@@ -223,12 +227,20 @@ def local_search_greedy1():
         while swap2 == swap1:
             swap2 = np.random.randint(0, K)
         
+        if len(router[swap1]) == 3:
+            a1 = 1
+            b1 = 2
+        else:
         #a1 != b1
-        a1 = np.random.randint(1, len(router[swap1]) - 2)
-        b1 = np.random.randint(a1 + 1, len(router[swap1]) - 1)
+            a1 = np.random.randint(1, len(router[swap1]) - 2)
+            b1 = np.random.randint(a1 + 1, len(router[swap1]) - 1)
 
-        a2 = np.random.randint(1, len(router[swap2]) - 2)
-        b2 = np.random.randint(a2 + 1, len(router[swap2]) - 1)
+        if len(router[swap2]) == 3:
+            a2 = 1
+            b2 = 2
+        else:
+            a2 = np.random.randint(1, len(router[swap2]) - 2)
+            b2 = np.random.randint(a2 + 1, len(router[swap2]) - 1)
 
         new_router1 = router[swap1][:a1] + router[swap2][a2:b2] + router[swap1][b1:]
         new_router2 = router[swap2][:a2] + router[swap1][a1:b1] + router[swap2][b2:]
@@ -249,7 +261,7 @@ def local_search_greedy1():
             history_count = 0
         else:
             history_count += 1
-        if history_count == 100:
+        if history_count == 1000:
             break
         
 
@@ -259,9 +271,106 @@ def local_search_greedy1():
         for pos in sub:
             print(pos, end = ' ')
         print()
-    return router, cost
+    return router, cost, K
 
-router, cost = local_search_greedy1()
+router, cost, K = local_search_greedy2()
+
+
+# import numpy as np
+
+# def cal_cost(router, t, d):
+#     cost = [0 for i in range(len(router))]
+#     for i in range(len(router)):
+#         for j in range(len(router[i]) - 1):
+#             cost[i] += t[router[i][j]][router[i][j + 1]] + d[router[i][j + 1]]
+#     return cost
+
+# def UCV(visited):
+#         unvisited = []
+#         for pos in range(len(visited)):
+#             if visited[pos] == 0:
+#                 unvisited.append(pos)
+#         return unvisited
+
+# def local_search_greedy1():
+#     N, K, d, t = read_data()
+#     router = []
+#     for i in range(K):
+#         router.append([0])
+
+#     visited = np.zeros(N + 1, dtype = int)
+#     visited[0] = 1      
+        
+#     k = 0
+#     while sum(visited) != N + 1:
+#         start = router[k][-1]
+#         next_pos = 0
+#         next_time = 1000000
+#         unvisited = UCV(visited)
+#         for pos in unvisited:
+#             if t[start][pos] + d[pos] < next_time:
+#                 next_time = t[start][pos] + d[pos]
+#                 next_pos = pos
+#         router[k].append(next_pos)
+#         visited[next_pos] = 1
+#         k = (k + 1) % K
+    
+#     for i in range(K):
+#         router[i].append(0)
+#     cost = [0 for i in range(K)]
+#     for i in range(K):
+#         for j in range(len(router[i]) - 1):
+#             cost[i] += t[router[i][j]][router[i][j + 1]] + d[router[i][j + 1]]
+#     history_count = 0
+#     while True:
+#         max_cost = max(cost)
+#         max_index = [i for i, j in enumerate(cost) if j == max_cost]
+#         router_neighborhood = []
+
+#         swap1 = np.random.choice(max_index)
+#         swap2 = np.random.randint(0, K)
+#         while swap2 == swap1:
+#             swap2 = np.random.randint(0, K)
+        
+#         #a1 != b1
+#         a1 = np.random.randint(1, len(router[swap1]) - 2)
+#         b1 = np.random.randint(a1 + 1, len(router[swap1]) - 1)
+
+#         a2 = np.random.randint(1, len(router[swap2]) - 2)
+#         b2 = np.random.randint(a2 + 1, len(router[swap2]) - 1)
+
+#         new_router1 = router[swap1][:a1] + router[swap2][a2:b2] + router[swap1][b1:]
+#         new_router2 = router[swap2][:a2] + router[swap1][a1:b1] + router[swap2][b2:]
+
+#         for i in range(K):
+#             if i == swap1:
+#                 router_neighborhood.append(new_router1)
+#             elif i == swap2:
+#                 router_neighborhood.append(new_router2)
+#             else:
+#                 router_neighborhood.append(router[i])
+        
+#         cost_neighborhood = cal_cost(router_neighborhood, t, d)
+#         new_max_cost = max(cost_neighborhood)
+#         if new_max_cost < max_cost:
+#             router = router_neighborhood
+#             cost = cost_neighborhood
+#             history_count = 0
+#         else:
+#             history_count += 1
+#         if history_count == 100:
+#             break
+        
+
+#     print(K)
+#     for sub in router:
+#         print(len(sub))
+#         for pos in sub:
+#             print(pos, end = ' ')
+#         print()
+#     return router, cost
+
+# router, cost = local_search_greedy1()
 
 
 
